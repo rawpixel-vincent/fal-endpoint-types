a21e0738f931ed75f64b4cd81fd2e2e946edad7d
rawpixel-vincent <vincent@rawpixel.com>
2026-02-06 18:56:11 +0700

    make types strict

--- types/fal/index.d.ts
+++ types/fal/index.d.ts
@@ -5,26 +5,30 @@ declare global {
         export type Endpoint = keyof fal.endpoints.Endpoints;
         export type EndpointInput<T extends fal.Endpoint> =
             T extends keyof fal.endpoints.Endpoints ? fal.endpoints.Endpoints[T]['input'] : never;
-        export type EndpointOutput<T extends fal.Endpoint> =
+        export type EndpointOutput<T extends keyof fal.endpoints.Endpoints> =
             T extends keyof fal.endpoints.Endpoints ? fal.endpoints.Endpoints[T]['output'] : never;
 
         /** Show all possible output properties for a set of endpoints. */
         export type EndpointsOutputsCombined<
             E extends fal.Endpoint,
-            T extends readonly fal.Endpoint[] = readonly [E, ...(readonly [E])],
-            O extends Record<string, never> = Record<string, never>,
+            T extends readonly fal.Endpoint[] = readonly [E, ...(readonly E[])],
+            O extends Partial<{ [key: string]: any }> = Partial<{ [key: string]: never }>,
         > =
-            T extends readonly [infer M, ...infer R extends readonly fal.Endpoint[]] ?
-                R extends never ?
-                    M extends fal.Endpoint ?
-                        Partial<fal.EndpointOutput<M>> & O
-                    :   O
-                :   EndpointsOutputsCombined<
-                        E,
-                        R,
-                        Partial<fal.EndpointOutput<M extends fal.Endpoint ? M : never>> & O
-                    >
-            :   O;
+            T extends readonly [infer M, ...infer R] ?
+                O &
+                    (M extends keyof fal.endpoints.Endpoints ? Partial<fal.EndpointOutput<M>>
+                    :   never) &
+                    (R extends readonly [] ?
+                        EndpointsOutputsCombined<
+                            E,
+                            readonly Exclude<R[number], M>[],
+                            O &
+                                (M extends keyof fal.endpoints.Endpoints ?
+                                    Partial<fal.EndpointOutput<M>>
+                                :   never)
+                        >
+                    :   O)
+            :   never;
     }
 }
 
